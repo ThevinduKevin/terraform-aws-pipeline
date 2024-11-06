79f96fafaa0ed075af01fd66f015bb6daaecd0cc
ThevinduKevin <thevindukevin@gmail.com>
2024-11-06 16:06:47 +0530

    new changes

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "terra-project-bucket"
-    key = "terraform/states/project-state.tfstate"
-    region = "ap-south-1"
+    bucket         = "terra-project-bucket"
+    key            = "terraform/states/project-state.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "terra-project-table"
-    encrypt = true
+    encrypt        = true
   }
 }
 
@@ -13,14 +13,14 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
-  vpc_cidr = "10.0.0.0/16"
+  source       = "./modules/vpc"
+  vpc_cidr     = "10.0.0.0/16"
   subnet_cidrs = ["10.0.1.0/24"]
 }
 
 module "ec2" {
- source = "./modules/ec2"
- subnet_id =  module.vpc.subnet_ids[0]
- instance_type = "t2.micro"
- ami_id = "ami-0c1a7f89451184c8b"
+  source        = "./modules/ec2"
+  subnet_id     = module.vpc.subnet_ids[0]
+  instance_type = "t2.micro"
+  ami_id        = "ami-0c1a7f89451184c8b"
 }
